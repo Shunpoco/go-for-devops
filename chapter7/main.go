b4a9b97defca4826495265ba224c52dd573e928d
Shunpoco <tkngsnsk313320@gmail.com>
2022-08-26 00:03:25 +0900

    stdin

--- chapter7/main.go
+++ chapter7/main.go
@@ -1,45 +1,42 @@
 package main
 
 import (
-	"flag"
+	"bufio"
 	"fmt"
 	"log"
 	"os"
+	"regexp"
 )
 
-var (
-	useProd = flag.Bool(
-		"prod",
-		true,
-		"Use a production endpoint",
-	)
-	useDev = flag.Bool(
-		"dev",
-		false,
-		"Use a development endpoint",
-	)
-	help = new(bool)
-)
-
-func init() {
-	flag.BoolVar(help, "help", false, "Display help text")
-	flag.BoolVar(help, "h", false, "Display help text (shorthand)")
-}
+var errRE = regexp.MustCompile(`(?i)error`)
 
 func main() {
-	flag.Parse()
-	if *help {
-		flag.PrintDefaults()
-		return
-	}
-
-	authors := flag.Args()
-	if len(authors) == 0 {
-		log.Println("did not pass any authors")
+	var s *bufio.Scanner
+	switch len(os.Args) {
+	case 1:
+		log.Println("No file specified, using STDIN")
+		s = bufio.NewScanner(os.Stdin)
+	case 2:
+		f, err := os.Open(os.Args[1])
+		if err != nil {
+			log.Println(err)
+			os.Exit(1)
+		}
+		s = bufio.NewScanner(f)
+	default:
+		log.Println("too many arguments provided")
 		os.Exit(1)
 	}
 
-	for _, author := range authors {
-		fmt.Println(author)
+	for s.Scan() {
+		line := s.Bytes()
+		if errRE.Match(line) {
+			fmt.Printf("%s\n", line)
+		}
+	}
+
+	if err := s.Err(); err != nil {
+		log.Println("Error: ", err)
+		os.Exit(1)
 	}
 }
