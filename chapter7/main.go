9bb53daaf8ab0929ce066aca40da4890dbd1fe12
Shunpoco <tkngsnsk313320@gmail.com>
2022-08-25 23:37:55 +0900

    custom flag

--- chapter7/main.go
+++ chapter7/main.go
@@ -3,8 +3,43 @@ package main
 import (
 	"flag"
 	"fmt"
+	"net/url"
+	"reflect"
 	"time"
 )
+
+// Custom flag
+// flag.Value interface is this:
+// type Value interface {
+// 	String() string
+// 	Set(string) error
+// }
+
+type URLValue struct {
+	URL *url.URL
+}
+
+func (v URLValue) String() string {
+	if v.URL != nil {
+		return v.URL.String()
+	}
+	return ""
+}
+
+func (v URLValue) Set(s string) error {
+	if u, err := url.Parse(s); err != nil {
+		return err
+	} else {
+		*v.URL = *u
+	}
+	return nil
+}
+
+var u = &url.URL{}
+
+func init() {
+	flag.Var(&URLValue{u}, "url", "URL to parse")
+}
 
 var endpoint = flag.String(
 	"endpoint",
@@ -33,6 +68,11 @@ var flagduration = flag.Duration(
 func main() {
 	flag.Parse()
 
+	if reflect.ValueOf(*u).IsZero() {
+		panic("did not pass an URL")
+	}
+
+	fmt.Printf("{scheme: %q, host: %q, path: %q}\n", u.Scheme, u.Host, u.Path)
 	fmt.Println("endpoint: ", *endpoint)
 	fmt.Println("flagbool: ", *flagbool)
 	fmt.Println("flagint: ", *flagint)
