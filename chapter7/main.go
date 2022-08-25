2a40a7a7a48a96826b40e4739bdcf4c80e7ce408
Shunpoco <tkngsnsk313320@gmail.com>
2022-08-25 23:46:06 +0900

    error handling

--- chapter7/main.go
+++ chapter7/main.go
@@ -2,79 +2,43 @@ package main
 
 import (
 	"flag"
-	"fmt"
-	"net/url"
-	"reflect"
-	"time"
+	"log"
+	"os"
 )
 
-// Custom flag
-// flag.Value interface is this:
-// type Value interface {
-// 	String() string
-// 	Set(string) error
-// }
-
-type URLValue struct {
-	URL *url.URL
-}
-
-func (v URLValue) String() string {
-	if v.URL != nil {
-		return v.URL.String()
-	}
-	return ""
-}
-
-func (v URLValue) Set(s string) error {
-	if u, err := url.Parse(s); err != nil {
-		return err
-	} else {
-		*v.URL = *u
-	}
-	return nil
-}
-
-var u = &url.URL{}
-
-func init() {
-	flag.Var(&URLValue{u}, "url", "URL to parse")
-}
-
-var endpoint = flag.String(
-	"endpoint",
-	"hoge.prd.com",
-	"The endpoint",
-)
-
-var flagbool = flag.Bool(
-	"flagbool",
-	false,
-	"flag bool",
-)
-
-var flagint = flag.Int(
-	"flagint",
-	100,
-	"flag int",
-)
-
-var flagduration = flag.Duration(
-	"flagduration",
-	time.Duration(100),
-	"flag duration",
+var (
+	useProd = flag.Bool(
+		"prod",
+		true,
+		"Use a production endpoint",
+	)
+	useDev = flag.Bool(
+		"dev",
+		false,
+		"Use a development endpoint",
+	)
+	help = flag.Bool(
+		"help",
+		false,
+		"Display help text",
+	)
 )
 
 func main() {
 	flag.Parse()
-
-	if reflect.ValueOf(*u).IsZero() {
-		panic("did not pass an URL")
+	if *help {
+		flag.PrintDefaults()
+		return
 	}
 
-	fmt.Printf("{scheme: %q, host: %q, path: %q}\n", u.Scheme, u.Host, u.Path)
-	fmt.Println("endpoint: ", *endpoint)
-	fmt.Println("flagbool: ", *flagbool)
-	fmt.Println("flagint: ", *flagint)
-	fmt.Println("flagduration: ", *flagduration)
+	switch {
+	case *useProd && *useDev:
+		log.Println("Error: --prod and --dev cannot both be set")
+		flag.PrintDefaults()
+		os.Exit(1)
+	case !(*useProd || *useDev):
+		log.Println("Error: either --prod or --dev must be set")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
 }
